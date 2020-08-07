c7cea0c1b31820de641cae7dbebf0aa7ed3b6616
verde <luzverde314@gmail.com>
2020-08-07 19:19:43 +0900

    계산기 기본 기능 구현 + ac기능

--- calcu/calcu-gui.js
+++ calcu/calcu-gui.js
@@ -1,99 +1,74 @@
+//input 입력 부분
 var input = {'array' : []};
-
 input.getInput = function(){
     return this.array.join("");
-}
+};
 
+//output 값 출력 부분
 var output = {};
 output.text = document.getElementById("output");
 
+//숫자와 연산자 버튼 클릭
 var clickNumbers = function(event){
     var str = event.target.innerHTML;
-    switch (str){
-        case 'BS':
+
+    if (str === 'BS'){
         input.array.pop();
-        break;
-        case '+':
-        input.array.push(' ' + str+ ' ');
-        break;
-        case '-':
-        input.array.push(' ' + str+ ' ');
-        break;
-        case '*':
-        input.array.push(' ' + str+ ' ');
-        break;
-        case '/':
-        input.array.push(' ' + str+ ' ');
-        break;
-        default:
-        input.array.push(str);       
+    } else if (str === '+' || str === '-' || str === '*' || str === '/'){
+        input.array.push(' ' + str + ' ');
+    } else {
+        input.array.push(str);
     }
+
     if(input.array.length == 0) {
         output.text.innerHTML = "Empty";
     }  else {
         output.text.innerHTML = input.getInput();
+    };
+
+};
+
+//입력값 배열로 만들기
+var spl = function(){
+    var ip = output.text.innerHTML;
+    var aa = ip.split(" ");  //배열로
+    return aa;
+};
+
+//계산
+var calculate = function(arr){
+    for(i = 1; i < (arr.length - i); i++){
+        switch(arr[(2 * i) - 1]){
+            case "+":
+            arr[0] = Number(arr[0]) + Number(arr[2 * i]);
+            break;
+
+            case "-":
+            arr[0] = Number(arr[0]) - Number(arr[2 * i]);
+            break;
+
+            case "*":
+            arr[0] = Number(arr[0]) * Number(arr[2 * i]);
+            break;
+
+            case "/":
+            arr[0] = Number(arr[0]) / Number(arr[2 * i]);
+            break;
+        };
     }
+};
 
-    // output.text.innerHTML = input.getInput();
-}
-
+//'=' 버튼
 var showResult = function(event){
-    console.log("showResult");
-    console.log(event.target.innerHTML);
-}
+    var ary = spl();
+    calculate(ary);
+    output.text.innerHTML = ary[0];
+};
 
+var allClear = function(event){
+    output.text.innerHTML = "0";
 
+};
 
-
-
-
-
-
-
-
-
-
-
-// function spl(){
-//     var ip = document.getElementById('ip1').value;
-//     var aa = ip.split(" ");  //배열
-//     return aa;
-// };   
-
-// function calculate(arr){
-//     for(i = 1; i < (arr.length - i); i++){
-//         switch(arr[(2 * i) - 1]){
-//             case "+":
-//             arr[0] = Number(arr[0]) + Number(arr[2 * i]);
-//             break;
-
-//             case "-":
-//             arr[0] = Number(arr[0]) - Number(arr[2 * i]);
-//             break;
-
-//             case "*":
-//             arr[0] = Number(arr[0]) * Number(arr[2 * i]);
-//             break;
-
-//             case "/":
-//             arr[0] = Number(arr[0]) / Number(arr[2 * i]);
-//             break;
-
-//             default:
-//             alert("알맞은 연산자를 입력해 주세요 + - * /");
-//             arr[0] = "연산자 입력 오류";
-//             break;
-//         };
-//     }
-// };
-
-// function print(v){
-//     return "최종 결과값은 " + v + "입니다.";
-// };
-
-// function main(){
-//     var ary = spl();
-//     calculate(ary);
-//     var out = document.getElementById('output');
-//     out.innerHTML = print(ary[0])
-// };   
+//추가로 필요한 기능
+//음수표현, 우선순위, 디자인
