827f50de5fa86af1ae244e09620d0d46b135f578
verde <luzverde314@gmail.com>
2020-08-08 23:35:31 +0900

    퍼즐구현3: 단어 섞기 랜덤으로 단어 뽑기, 글자 섞기

--- puzzle/puzzle.js
+++ puzzle/puzzle.js
@@ -1,41 +1,64 @@
-var str = document.getElementById("word1").innerHTML;
-var word2 = document.getElementById("word2");
+var ranList = ["EAGLE", "TIGER", "ELEPHANT", "GIRAFFE", "OSTRICH",
+                "LIZARD", "DRAGON", "PELICAN", "FLAMINGO", "PENGUIN"];
+var pick = function(event){
+    var ranPick = ranList[Math.floor(Math.random() * ranList.length)];
+    word1.innerHTML = ranPick;
+    return word1.innerHTML;
+};
+var pickSpl = pick().split("");
 
-var game = {};
-game.word = str.split("");
-game.btns = [];
-game.copyBtnText = function(){
-    for (var i = 0; i < this.word.length; i++) {
-        this.btns[i].innerHTML = this.word[i];
+var btnVal = [];
+var copyBtnText = function(){
+    for(var i = 0; i < pickSpl.length; i++){
+        btnVal[i].innerHTML = pickSpl[i];
     }
 };
 
-for (var i = 0; i < str.length; i++) {
-    var btn = document.createElement("button");
-    btn.innerHTML = str[i];
-    word2.appendChild(btn);
-    game.btns.push(btn);
+var createBtn = function(){
+    for (var i = 0; i < pickSpl.length; i++){
+        var btn = document.createElement("button");
+        btn.innerHTML = pickSpl[i];
+        word2.appendChild(btn);
+        btnVal.push(btn);
+    }
+};
+createBtn();
+
+var ranran = function(){
+    Math.floor(Math.random() * 5)
+    return Math.floor(Math.random() * 5);
+}
+var shuffle = function(){
+    while(ranran() === 1){
+        swap();
+    };
+    while(ranran() === 2){
+        shiftR();
+    };
+    while(ranran() === 3){
+        shiftL();
+    };
 };
 
 var swap = function (event) {
-    game.word.reverse();
-    game.copyBtnText();
+    pickSpl.reverse();
+    copyBtnText();
 };
 
 var shiftR = function (event) {
-    game.word.unshift(game.word.pop());
-    game.copyBtnText();
+    pickSpl.unshift(pickSpl.pop());
+    copyBtnText();
 };
 
 var shiftL = function (event) {
-    game.word.push(game.word.shift());
-    game.copyBtnText();
+    pickSpl.push(pickSpl.shift());
+    copyBtnText();
 };
 
 var judge = function(event){
-    if(word1.innerHTML === game.word.join("")){
+    if(word1.innerHTML === pickSpl.join("")){
         alert("단어 맞추기를 성공하셨습니다!");
     } else {
         alert("다시 해보세요");
     }
-}
+};
